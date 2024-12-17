87cb55ef758fa2b2145709b3ee29f4db6a59ba6b
thorfinn-karlsefn <andrei.ott@docusketch.com>
2024-12-17 23:14:32 +0400

    add mobile nav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -16,7 +16,7 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
       <Sidebar {...currentUser} />
 
       <div className="flex h-full flex-1 flex-col">
-        <MobileNavigation />
+        <MobileNavigation {...currentUser} />
         <Header />
 
         <div className="main-content">{children}</div>
