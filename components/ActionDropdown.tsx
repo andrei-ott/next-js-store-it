ac0520e5455e58d96c17d5a2231ab4ea0a06596d
thorfinn-karlsefn <andrei.ott@docusketch.com>
2024-12-19 18:30:42 +0400

    create file card

---
+++ components/ActionDropdown.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Models } from "node-appwrite";
+
+const ActionDropdown = ({ file }: { file: Models.Document }) => {
+  return <div>ActionDropdown</div>;
+};
+
+export default ActionDropdown;
